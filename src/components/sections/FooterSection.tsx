e0e03d28013878a162cb685d5ad2695b3b925421
Ines <ines_gbadamassi@outlook.fr>
2024-08-09 23:26:49 +0200

    Update Footer Section

--- src/components/sections/FooterSection.tsx
+++ src/components/sections/FooterSection.tsx
@@ -1,38 +1,21 @@
 import React from "react";
 
 function FooterSection() {
-  const product = ["Features", "Pricing", "Changelog"];
-  const company = ["Manifest", "Community", "Careers", "Contact"];
-  const resources = [
-    "Help center",
-    "Report bug",
-    "Terms of use",
-    "Privacy policy",
-  ];
   return (
-    <section className="bg-customDark pt-10 pb-10 text-white flex justify-around gap-8 px-8 flex-col md:flex-row lg:px-[20rem]">
-      <div>
-        {/* <img src="/images/footer_logo.png" alt="footer logo" /> */}
+    <section className="bg-customDark pt-10 pb-10 text-white flex justify-center items-center gap-2 px-4 flex-col md:flex-row">
+      <div className="flex items-center gap-4">
         <p className="text-customGray font-bold">
           The chatbot assistant that works for you
         </p>
-      </div>
-      <div className="flex-grow flex justify-around flex-col gap-8 md:flex-row">
-        <div>
-          <div>
-            <p className="font-bold">Github</p>
-          </div>
-        </div>
-        <div>
-          <div>
-            <p className="font-bold">Powered by OpenAI</p>
-          </div>
-        </div>
-        <div>
-          <div>
-            <p className="font-bold">Resources</p>
-          </div>
-        </div>
+        <a href="https://github.com/I2S9/customerwise-ai" target="_blank" rel="noopener noreferrer" className="font-bold text-white hover:underline">
+          Github
+        </a>
+        <a href="https://www.openai.com" target="_blank" rel="noopener noreferrer" className="font-bold text-white hover:underline">
+          Powered by OpenAI
+        </a>
+        <a href="https://www.google.com/" target="_blank" rel="noopener noreferrer" className="font-bold text-white hover:underline">
+          Resources
+        </a>
       </div>
     </section>
   );
